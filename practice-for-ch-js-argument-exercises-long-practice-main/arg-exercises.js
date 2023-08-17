b8c197fd39b868ef5f2e75c271fc61f98def56a6
elevenstand7 <elevenstand7@gmail.com>
2023-08-17 15:11:31 -0400

    almost mybind

--- practice-for-ch-js-argument-exercises-long-practice-main/arg-exercises.js
+++ practice-for-ch-js-argument-exercises-long-practice-main/arg-exercises.js
@@ -6,8 +6,8 @@ function sum() {
     return totalSum;
 };
 
-console.log(sum(1, 2, 3, 4))
-console.log(sum(1, 2, 3, 4, 5))
+// console.log(sum(1, 2, 3, 4))
+// console.log(sum(1, 2, 3, 4, 5))
 
 function sum2(...args) {
     let totalSum = 0;
@@ -17,8 +17,8 @@ function sum2(...args) {
     return totalSum;
 };
 
-console.log(sum2(1, 2, 3, 4))
-console.log(sum2(1, 2, 3, 4, 5))
+// console.log(sum2(1, 2, 3, 4))
+// console.log(sum2(1, 2, 3, 4, 5))
 
 function sum3() {
     let totalSum = 0;
@@ -29,6 +29,38 @@ function sum3() {
     return totalSum;
 };
 
-console.log(sum3(1, 2, 3, 4))
-console.log(sum3(1, 2, 3, 4, 5))
+// console.log(sum3(1, 2, 3, 4))
+// console.log(sum3(1, 2, 3, 4, 5))
 
+Function.prototype.myBind = function(ctx){
+    let that = this;
+    let args = Array.prototype.slice(1).call(arguments);
+    return function (){
+        let args2 = Array.prototype.slice.call(arguments);
+        return that.apply(ctx, args.concat(args2));
+    }
+}
+
+class Cat {
+    constructor(name) {
+      this.name = name;
+    }
+
+    says(sound, person) {
+      console.log(`${this.name} says ${sound} to ${person}!`);
+      return true;
+    }
+  }
+
+  class Dog {
+    constructor(name) {
+      this.name = name;
+    }
+  }
+
+  const markov = new Cat("Markov");
+  const pavlov = new Dog("Pavlov");
+
+  markov.says("meow", "Ned");
+
+  console.log(markov.says.myBind(pavlov, "meow", "Kush")());
